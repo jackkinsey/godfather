664ea49b71b1f2eb430e3cc5b0dbad9dbcde3545
ctofani42 <ctofani42@gmail.com>
2017-04-18 19:40:35 -0400

    trying Xcode again

---
+++ godfather/godfather/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  godfather
+//
+//  Created by Chris Tofani on 4/18/17.
+//  Copyright © 2017 Chris Tofani. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
