1a13f58b282fcc337b043dd2b158d52f818b3854
warneracw21 <warneracw@gmail.com>
2017-04-23 16:43:48 -0400

    Verbose Reading of CSV File

--- godfather/godfather/input.cpp
+++ godfather/godfather/input.cpp
@@ -2,3 +2,29 @@
  * input.cpp
  * Processess CSV files into command structs.
  */
+
+#include <stdlib.h> //For malloc, etc.
+#include <string.h> //For memcpy.
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "input.h"
+using namespace std;
+
+AirportInputProcessor::readCSV() {
+
+	string line;
+    ifstream myfile ("data.csv");
+    if (myfile.is_open())
+    {
+        cout <<"File is Open"<<"\n";
+        while ( getline (myfile,line) )
+        {
+            cout << line << '\n';
+        }
+        myfile.close();
+    }
+    
+    else cout << "Unable to open file";
+
+}
