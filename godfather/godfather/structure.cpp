348b5a12b4fd122428133c93873c0562cb6b60f3
Jack Kinsey <j.jameskinsey@gmail.com>
2017-04-23 19:35:21 -0400

    added pseudocode to the Timeline methods

--- godfather/godfather/structure.cpp
+++ godfather/godfather/structure.cpp
@@ -11,7 +11,13 @@ struct IndexNode {
     DynamicArray<struct Plane>* data;
 };
 
-struct IndexNode* Timeline::createIndex(int index) {
+struct IndexNode* Timeline::createIndex(int depth, int index) {
+    /*
+     * IndexNode newTime = {};
+     * newTime.index = index;
+     * newTime.data = new DynamicArray<struct Plane>;
+     * center.insert(depth, newTime);
+     */
     return NULL;
 }
 
@@ -20,13 +26,28 @@ Timeline::Timeline() {
 }
 
 Timeline::~Timeline() {
+    /*
+     * for all nodes in center:
+     *  delete node->datum->data
+     */
     delete this->_center;
 }
 
 struct IndexNode* Timeline::fetch(int depth) {
+    /*
+     * return center[depth]
+     */
     return NULL;
 }
 
 bool Timeline::push(int index, struct Plane& el) {
+    /*
+     * while center[i].index < index:
+     *  i++
+     * if center[i].index==index:
+     *  center[i].data.append(el);
+     * else:
+     *  createIndex(i, index).data.append(el);
+     */
     return NULL;
 }
